4675807dcad96571012674fcff6abb40d6b5a1cf
LucaLogallo <lucalogallo1998@gmail.com>
2021-04-19 16:07:41 +0200

    esercizio finito

---
+++ main.js
@@ -0,0 +1,16 @@
+$( document ).ready(function(){
+  var hamburgerIconOpen = $('.header-right > a'); //referenzio il favicon dell'hamburger per aprirlo
+  var hamburgerIconClose = $(".hamburger-menu .close");//referenzio il favicon dell'hamburger per chiuderlo
+  var hamburger = $('.hamburger-menu');//referenzio l'hamburger menù di default display none 
+ 
+  hamburgerIconOpen.click(apri); //richiamo la funzione apri quando avviene il click sull'hamburgerIconOpen referenziato con il favicon dell'hamburger per aprirlo
+  hamburgerIconClose.click(chiudi);//richiamo la funzione chiudi quando avviene il click sull'hamburgerIconClose referenziato con il favicon dell'hamburger per chiuderlo
+
+  function apri(){ //funzione che apre l'hamburger di default display none con un fadeIn di 1 secondo
+    hamburger.fadeIn(1000);
+  }
+  function chiudi(){//funzione che chiude l'hamburger di default display none con un fadeOut di 1 secondo
+    hamburger.fadeOut(1000);
+  }
+
+});
